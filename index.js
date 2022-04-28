ec59917a2d4d7c05fff0eba7948de2de91532c02
nicholasdunbar <nicholasdunbar@gmail.com>
2022-04-27 23:52:24 -0600

    migrate everything to be template based

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var topnav;
 var header;
-var sectionTemplate;
+var store = {};
 //old landing pages for reference:
 //https://forms.gle/zi1pgjQTtABAdTNN9
 //https://calendly.com/hypothesiscoaching/15-minute-free-consultation
@@ -9,7 +9,9 @@ var getStartedURL = "https://calendly.com/hypothesiscoaching/15-minute-free-cons
 var baseURL = window.location.protocol+"//"+window.location.hostname;
 baseURL += window.location.port != 80 ? ':'+window.location.port : '';
 baseURL += "/";
-
+function getScheduleURL(){
+  return getStartedURL;
+}
 //used to load css sheets dynamically
 function loadStyle(src) {
       let link = document.createElement('link');
@@ -34,116 +36,89 @@ function jumpToTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-//navigate to form to apply
-function getStartedBtnFn(){
-  window.location.href = getStartedURL;
+//return promise that when ran will load json content
+function getJSONFn(i){
+  var key = i+"-content.json";
+  if (key in store){
+    return new Promise((resolve) => resolve (store[key]));
+  }
+  return fetch(baseURL+key)
+    .then((response) => response.json())
+    .then((response) => {
+      store[key] = response;
+      return store[key];
+    });
 }
-function loadContent(json){
-  var n;
-  var section;
-  var sectionLast;
-  var sectionClone;
-  if (json.header.isShow && !document.body.contains(header)){
-    topnav.after(header);
-  } else if (!json.header.isShow && document.body.contains(header)) {
-    header.remove();
+//return promise to get, populate, and render template
+function getTemplateFn(i, contentObj = undefined){
+  var key = i+"-template.html";
+  if (key in store){
+    return new Promise( resolve => resolve(store[key]) )
+    .then( response => loadHTMLContent (response, contentObj) );
   }
-  if (json.header.isShow){
-    initHeader(json.header.h, json.header.p, json.header.link);
-  }
-  jumpToTop();
-  for (var i = 0; i < json.sections.length; i++){
-    n = i+1;
-    section = document.querySelector("#section"+n);
-    if(document.body.contains(section)){
-      sectionLast = section;
-    } else {
-      //add section
-      sectionClone = sectionTemplate.cloneNode(true);
-      sectionClone.setAttribute("id", "section"+n);
-      sectionLast.after(sectionClone);
-      sectionLast = sectionClone;
+  return fetch(baseURL+"/templates/"+key)
+    .then(response => response.text())
+    .then((response) => {
+      store[key] = response;
+      return response;
+    })
+    .then(response => loadHTMLContent (response, contentObj));
+}
+//return function to load HTML content at the bottom of the page
+function getHTMLContentFn(i){
+  
+    var key = i+"-template.html";
+    if (key in store){
+      return new Promise(resolve => resolve (store[key]))
+      .then(response => loadHTMLContent(response));
     }
-    initText("#section"+n, json.sections[i].h, json.sections[i].d, json.sections[i].p, json.sections[i].glyph);
-
-  }
-  clearSectionsAfter(++n);
-
-  initQuote(json.quote);
+    return fetch(baseURL+key)
+    .then(response => response.text())
+    .then(response => {
+      store[key] = response;
+      return response;
+    })
+    .then(response => loadHTMLContent (response));
 }
-//get function that when ran will load json content
-function getContentFn(i){
-  return function(){
-    fetch(baseURL+i+"-content.json")
-    .then(function(response){return response.json()})
-    .then(loadContent);
-  }
-}
-//get function that when ran will load HTML content
-function getTemplateFn(i){
-  return function(){
-    fetch(baseURL+i+"-template.html")
-    .then(function(response){return response.text()})
-    .then(loadHTMLContent);
-  }
-}
-function loadHTMLContent(html){
-  console.log(html);
-  header.remove();
-  clearSectionsAfter(1);
-  const quote = document.querySelector('#quote');
-  quote.remove();
-  const headerNode = document.querySelector('#topnav');
+function loadHTMLContent(html, contentObj){
+  const allSections = document.querySelectorAll('#template, #topnav, section, header');
+  const lastNode = allSections[allSections.length-1]; 
   const newNode = document.createElement('div');
-  newNode.innerHTML = html;
-  headerNode.after(newNode);
-}
-function initNavButtons(linkNames, highlight){
-  var links1 = document.querySelectorAll("#nav1 a");
-  var links2 = document.querySelectorAll("#nav2 a");
-  //set first home link
-  //links1[1].addEventListener("click", setHomeContent);
-  links1[1].classList.remove("w3-white");
-  if (highlight == -1){
-    //highlight the home link in the nav
-    links1[1].classList.add("w3-white");
+  if (contentObj !== undefined){
+    for (const [key, value] of Object.entries(contentObj)){
+      html = html.replaceAll(`{{${key}}}`, value);
+    }
   }
-  for (var n = 2; n <= linkNames.length+1; n++){
-    links1[n].innerText = linkNames[n-2];
-    //links1[n].addEventListener("click", getContentFn(n-2));
+  newNode.innerHTML = html;
+  lastNode.after(newNode);
+  let anchorEl;
+  anchorEl = newNode.querySelector('a.get-started');
+  if (anchorEl) anchorEl.href = getStartedURL;
+}
+function initNavButtons(highlight){
+  var links1 = document.querySelectorAll("#nav1 a");
+  for (var n = 1; n < links1.length; n++){
     links1[n].classList.remove("w3-white");
-    if (n-2 == highlight){
+    if (links1[n].textContent.toLowerCase() === highlight){
+      //highlight the link in the nav
       links1[n].classList.add("w3-white");
     }
   }
-  for (n = 0; n < linkNames.length; n++){
-    links2[n].innerText = linkNames[n];
-    //links2[n].addEventListener("click", getContentFn(n));
-    //links2[n].classList.remove("w3-white");
-    //if (n == highlight){
-    //  links2[n].classList.add("w3-white");
-    //}
-  }
+
 }
 //update Header
 function initHeader(h, p, l){
   getStartedURL = l;
   header.querySelector('h1').innerHTML = h;
   header.querySelector('p').innerText = p;
-  header.querySelector('button').removeEventListener("click", getStartedBtnFn);
-  header.querySelector('button').addEventListener("click", getStartedBtnFn);
 }
 //remove sections after n
 function clearSectionsAfter(n){
   //clear unnecessary sections
   var isFound = true;
   var section;
-  try {
-    section = document.querySelector("#section"+n);
-  } catch(e){
-    isFound = false;
-  }
-  while(isFound && document.body.contains(section)){
+  section = document.querySelector("#section"+n);
+  while(document.body.contains(section)){
     section.remove();
     n++;
     section = document.querySelector("#section"+n);
@@ -189,42 +164,6 @@ function initQuote(qText){
   var qEl = document.querySelector("#quote");
   qEl.innerText = qText;
 }
-function setHomeContent(){
-  highlight = -1;
-  if (!document.body.contains(header)){
-    topnav.after(header);
-  }
-  initHeader('There is a new way<br/>to transform your life.',"You don't need WILL Power, you need WHY power.", getStartedURL);
-  clearSectionsAfter(3);
-  initText( "#section1",
-            "Will Power Alone Doesn't Work",
-
-            "We've all heard the Nike ad. Just do it. After decades of advertising "+
-            "70% of Americans have obesity or are overweight. Will power doesn't work. It is time for a new approach.",
-
-            "Start by asking WHY you want to make a change in your life. "+
-            "Coaching uses psychological techniques that help stimulate a person's motivation. "+
-            "Why Power Coaches are trained in an evidence based technique called Motivational Interviewing (MI). "+
-            "Its effectiveness has been validated in 200 randomized control studies - "+
-            "the highest quality evidence science has to offer. ",
-            "section1-icon"
-          );
-  initText( "#section2",
-            "What We Do",
-
-            "We are ACE certified, health and wellness life coaches. We specialize in helping sixteen to early-twenties boys "+
-            "with Life Direction Coaching, people suffering from depression, "+
-            "and people diagnosed with Fatty Liver Disease. "+
-            "Click the links in the menu bar for more information",
-
-            "Good coaches interrupt unproductive thought patterns. "+
-            "They help you creatively explore and unearth your deepest motivations. "+
-            "They teach personal-organizational strategies, but most of all, they're there for you, so you don't have to struggle alone. "+
-            "<br><br>Contact us now, <a  class=\"get-started\" href=\"https://forms.gle/zi1pgjQTtABAdTNN9\">click here to get started</a><br>Download <a href=\"https://docs.google.com/document/d/1MPu0u-vEvTZePmOS1k911bH41HvpvJE1B0SSwyxkqoU/edit?usp=sharing\">brochure here</a><br><br>* We are not medical professionals. We are behavior change experts. We do not diagnose or prescribe. Coaching for NAFLD requires an integrated approach with a registered dietitian, functional medical doctor, gastroenterologist, or other qualified MD. Coaching for depression requires you are also seeing a licensed therapist or state certified mental health practitioner. <a href=\"disclaimer.html\">See disclaimer here.</a>",
-            "section2-icon"
-          );
-  initQuote("Do what you can, with what you have, where you are. – Theodore Roosevelt");
-}
 
 //load all the css styles
 loadStyle("https://www.w3schools.com/w3css/4/w3.css");
@@ -242,18 +181,11 @@ var highlight = null;
 function initPage(){
   topnav = document.querySelector("#topnav");
   header = document.querySelector("header");
-  sectionTemplate = document.querySelector("#section3");
   jumpToTop();
 }
 function initNav(whichNavHighlight){
   highlight = whichNavHighlight;
-  initNavButtons(new Array(
-    "Depression",
-    "Life Direction Coaching",
-    "Fatty Liver Disease",
-    "About"),
-    highlight
-  );
+  initNavButtons(highlight);
 }
 //meta pixel code:
 !function(f,b,e,v,n,t,s)
@@ -269,6 +201,6 @@ function initNav(whichNavHighlight){
 //:end meta pixel code
 
 // export as Node module /  browser variable
-const toolbox = {initNav, initPage, getContentFn, getTemplateFn, setHomeContent, toggleMenu};
+const toolbox = {initNav, initPage, getHTMLContentFn, getTemplateFn, getJSONFn, toggleMenu, initQuote, getScheduleURL};
 if (typeof exports === 'object' && typeof module !== 'undefined') module.exports = {toolbox};
-else window.toolbox = toolbox;
+else window.toolbox = toolbox; 

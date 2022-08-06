d0fdffd33a832d4d56af84b863efbbf7f7b43164
nicholasdunbar <nicholasdunbar@gmail.com>
2022-08-05 18:16:47 -0600

    disable pixel

--- index.js
+++ index.js
@@ -175,16 +175,17 @@ function initNav(whichNavHighlight){
   initNavButtons(highlight);
 }
 //meta pixel code:
-!function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '312932777447892');
-  fbq('track', 'PageView');
+//comment for now, we may only need the calendly integration
+// !function(f,b,e,v,n,t,s)
+//   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+//   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+//   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+//   n.queue=[];t=b.createElement(e);t.async=!0;
+//   t.src=v;s=b.getElementsByTagName(e)[0];
+//   s.parentNode.insertBefore(t,s)}(window, document,'script',
+//   'https://connect.facebook.net/en_US/fbevents.js');
+//   fbq('init', '312932777447892');
+//   fbq('track', 'PageView');
 //:end meta pixel code
 
 // export as Node module /  browser variable

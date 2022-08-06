122048fc5a3704c67438594f9dd220f871058b1a
nicholasdunbar <nicholasdunbar@gmail.com>
2022-08-05 18:04:12 -0600

    use original pixel

--- index.js
+++ index.js
@@ -175,18 +175,7 @@ function initNav(whichNavHighlight){
   initNavButtons(highlight);
 }
 //meta pixel code:
-//old pixel that is not part of meta account
-// !function(f,b,e,v,n,t,s)
-//   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-//   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-//   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-//   n.queue=[];t=b.createElement(e);t.async=!0;
-//   t.src=v;s=b.getElementsByTagName(e)[0];
-//   s.parentNode.insertBefore(t,s)}(window, document,'script',
-//   'https://connect.facebook.net/en_US/fbevents.js');
-//   fbq('init', '312932777447892');
-//   fbq('track', 'PageView');
-  !function(f,b,e,v,n,t,s)
+!function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -194,7 +183,7 @@ function initNav(whichNavHighlight){
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1202023500579533');
+  fbq('init', '312932777447892');
   fbq('track', 'PageView');
 //:end meta pixel code
 

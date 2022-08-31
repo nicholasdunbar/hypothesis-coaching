11620be569dfa3cb5b45585de4a47d4d167a73eb
nicholasdunbar <nicholasdunbar@gmail.com>
2022-08-31 11:29:03 -0600

    enable google tag manager

--- index.js
+++ index.js
@@ -211,13 +211,13 @@ import('https://www.googletagmanager.com/gtag/js?id=G-0QNG5P2408')
 } ).then(
   //Google tagmanager
   () => {
-    if (params.get('debug') == 'true') {
+    //if (params.get('debug') == 'true') {
       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-WN37N2B');
-    } 
+    //} 
   }
   //:end Google tagmanager
 )
